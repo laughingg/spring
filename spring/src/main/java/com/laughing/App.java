35e2533b6b663cea488de5a0213200e5ee68e352
laughing <laughing@laughingdeiMac.local>
2018-09-05 15:48:24 +0800

    添加spring 项目到仓库

---
+++ spring/src/main/java/com/laughing/App.java
@@ -0,0 +1,13 @@
+package com.laughing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
